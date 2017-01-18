9a7bf4ab45d1b83adc7f62097d5a2e66473bf8b5
JoscelineO <josceline@mac.com>
2017-01-18 10:08:37 +0000

    prep for heroku

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: 'mongodb://localhost/my-city-guides',
+  db: process.env.MONGODB_URI || 'mongodb://localhost/my-city-guides',
   secret: process.env.SECRET || 'my heart is in Berlin'
 };
